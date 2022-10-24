3bd2c394b7fd77026142f3d15d1ccdbe592bc90d
Arrieta <albert.arrieta@uleth.ca>
2022-10-24 13:04:27 -0600

    Added levels and settings page. Icons, First level page.

--- www/js/index.js
+++ www/js/index.js
@@ -13,6 +13,10 @@ var app = new Framework7({
           path: '/settings/',
           url: 'pages/settings.html',
         },
+        {
+            path: '/level1/',
+            url: 'pages/level1.html',
+        },
       ],
   });
 var mainView = app.views.create('.view-main')

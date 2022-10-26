3c851f7dfe2d5fcd4da963f6acda9755499a59cd
Arrieta <albert.arrieta@uleth.ca>
2022-10-26 13:25:25 -0600

    trying to get submit to work

--- www/js/game.js
+++ www/js/game.js
@@ -1,5 +1,31 @@
 var coins = 0;
+var progress = 1;
+
 var $$ = Dom7;
+var levels = [level1, level2] ;
+
+//levels
+
+var level1 = {num: 1, 
+    riddle: 'What has to be broken before you can use it?', 
+    answer: 'egg',
+    hint1: 'It is a food',
+    hint2: 'A breakfast Food',
+    hint3: '3 letter word',
+}
+
+var level2 = {num: 2, 
+    riddle: 'What can run but cannot walk?', 
+    answer: 'river', 
+    hint1: 'Not an animal',
+    hint2: 'Can be found outdoors',
+    hint3: 'Made up of water',
+}
+
+//-----------------------------------------------
+function levelup() {
+    progress++;
+}
 
 function addcoins(amount) {
     coins += amount;
@@ -11,10 +37,13 @@ function resetGame() {
     $(".coins").html(coins);
 }
 
-//Script for the whole App
+
+
+//Global Script for all pages
 $$(document).on('page:init', function(e) {
     $(".coins").html(coins);
 
+    //settings page
     $("#add100").on("click", function () {
         addcoins(100);
         console.log("ADDING COINS")
@@ -24,5 +53,20 @@ $$(document).on('page:init', function(e) {
         resetGame();
         console.log("Game Reset")
     });
+
+
+    //level page
+    $('#levelnum').html(level1.num);
+    $('#riddle').html(level1.riddle);
+    $('#submit').on("click", function () {
+        if ($("#answerInput").text() == level1.answer) {
+            console.log("CORRECT");
+        } else {
+            console.log("INCORRECT: " + $("#answerInput").text());
+        }
+    })
+
+
 })
+
 

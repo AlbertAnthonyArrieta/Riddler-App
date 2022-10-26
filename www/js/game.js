f6e4db8b5bf53c291439ac4e8059d5ae9e521aaf
Arrieta <albert.arrieta@uleth.ca>
2022-10-26 11:32:17 -0600

    got script to work on multi pages

--- www/js/game.js
+++ www/js/game.js
@@ -1,7 +1,15 @@
 var coins = 0;
+var $$ = Dom7;
 
 function addcoins(amount) {
     coins += amount;
 }
 
-$(#add100).on("click", addcoins(100));
+addcoins(200);
+
+
+$$(document).on('page:init', function(e) {
+    $(".coins").html(coins);
+})
+
+$("#add100").on("click", addcoins(100));
